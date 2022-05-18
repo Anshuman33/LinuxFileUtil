8e91249c2bfaa3b7045c7dc8c40c53b0f9a8f4f9
Anshuman Gupta <gupta.anshuman20@gmail.com>
2022-05-19 00:19:14 +0530

    Completed headers for pipes

--- linuxFileUtil/pipes.h
+++ linuxFileUtil/pipes.h
@@ -1,5 +1,20 @@
 #include <sys/stat.h>
 
-extern int openNamedPipe(const char * path, mode_t mode);
+// Opens a named pipe
+extern int openNPipe(const char * path, mode_t mode);
 
-extern void getNamedPipeData(int fd);
+// Reads data from named pipe
+extern void readNPipe(int fd);
+
+// Writes data to named pipe
+extern void writeNPipe(int fd, char * data, int n);
+
+// Opens an unnamed pipe
+extern void openPipe(int fds[]);
+
+// Reads data from pipe
+extern void readPipe(int readfd);
+
+// Writes data in unnamed pipe
+extern void writePipe(int writefd, char * data, int n);
+
